042623f0375b48a7c320b665e991e14b986bed9f
Luke Rodgers <luke.rodgers@gene.co.uk>
2026-01-12 10:15:04 +0000

    Debug install

--- .github/post-project.sh
+++ .github/post-project.sh
@@ -3,23 +3,8 @@ set -v
 pwd
 ls -lasth
 cat composer.json
-sleep 1
 
-echo "start - attempting to fix up the composer repositories"
-composer --version
-composer config --unset repo.0
-cat composer.json
-sleep 1
-composer config --unset repositories.0
-cat composer.json
-sleep 1
-echo "asdfasdfads" >> composer.json
-sleep 1
-cat composer.json
-echo "end -attempting to fix up the composer repositories"
-
-
-
-
+# https://github.com/composer/composer/issues/12623#issuecomment-3551953185
+composer config --unset repositories
 
 set +v
