a6b7f388faf1e05fcce086ffe064f0f92f3f5d20
Luke Rodgers <luke.rodgers@gene.co.uk>
2026-01-12 10:08:23 +0000

    Debug install

--- .github/pre-install.sh
+++ .github/pre-install.sh
@@ -8,6 +8,8 @@ echo "start - attempting to fix up the composer repositories"
 composer --version
 composer config --unset repo.0
 cat composer.json
+composer config --unset repositories.0
+cat composer.json
 echo "end -attempting to fix up the composer repositories"
 
 set +v
