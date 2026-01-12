270bda07c5dde0b9a68c7a04fcce1828d84e6c23
Luke Rodgers <luke.rodgers@gene.co.uk>
2026-01-12 11:08:07 +0000

    Debug install

--- .github/post-project.sh
+++ .github/post-project.sh
@@ -5,6 +5,6 @@ ls -lasth
 cat composer.json
 
 # https://github.com/composer/composer/issues/12623#issuecomment-3551953185
-composer config --unset repositories
+#composer config --unset repositories
 
 set +v
